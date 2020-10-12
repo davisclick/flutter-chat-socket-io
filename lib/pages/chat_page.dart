49d0861b18e37c82746f84fd098fcf3a546fb15f
Davis Álvarez <alvarez.davis@gmail.com>
2020-10-12 18:13:25 -0500

    Starting chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -6,9 +6,45 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('ChatPage'),
-     ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text( 'Te', style: TextStyle(fontSize: 12) ),
+              backgroundColor: Colors.blue[100],
+              maxRadius: 14,
+            ),
+            SizedBox( height:3 ),
+             Text('Davis Alvarez', style: TextStyle( color: Colors.black87, fontSize: 12),)
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+
+     body: Container(
+        child: Column(
+          children:<Widget> [
+            Flexible(
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                itemBuilder: ( _, i) => Text('$i'),
+                reverse: true,
+              ),
+            ),
+            Divider(
+              height:1
+            ),
+            Container(
+              color: Colors.white,
+              height: 100,
+            )
+
+          ]
+        ),
+    )
+      
    );
   }
 }
